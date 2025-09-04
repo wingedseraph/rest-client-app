6ac3bdb0ce2335c7628cfa0efb7caf9bbd89153e
Rob Marvin <fenalfan@gmail.com>
2025-09-04 18:23:50 +0300

    feat: add next-intl config files

---
+++ src/i18n/navigation.ts
@@ -0,0 +1,5 @@
+import { routing } from './routing'
+import { createNavigation } from 'next-intl/navigation'
+
+export const { Link, redirect, usePathname, useRouter, getPathname } =
+  createNavigation(routing)
