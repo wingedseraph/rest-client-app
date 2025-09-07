2101f03b37a72fb8ef9bb9329f8632e5f038aba4
Diana Solovey <93824073+rustytrooper@users.noreply.github.com>
2025-09-07 16:25:12 +0000

    feat: configure db integaretion

---
+++ firebase.js
@@ -0,0 +1,67 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  sendPasswordResetEmail,
+  signOut,
+} from "firebase/auth";
+import {
+  getFirestore,
+  setDoc,
+  doc
+} from "firebase/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyARQOEJ8GQZdL3RjjvtFerjpb5oZtlTCLA",
+  authDomain: "restclient-project.firebaseapp.com",
+  projectId: "restclient-project",
+  storageBucket: "restclient-project.firebasestorage.app",
+  messagingSenderId: "570963669477",
+  appId: "1:570963669477:web:64102d8434480178d2ff0c"
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const logInWithEmailAndPassword = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const registerWithEmailAndPassword = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await setDoc(doc(db, "users", user.uid), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const sendPasswordReset = async (email) => {
+  try {
+    await sendPasswordResetEmail(auth, email);
+    alert("Password reset link sent!");
+  } catch (err) {
+    console.error(err);
+    alert(err.message);
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export {
+  auth,
+  db,
+  logInWithEmailAndPassword,
+  registerWithEmailAndPassword,
+  sendPasswordReset,
+  logout,
+};
