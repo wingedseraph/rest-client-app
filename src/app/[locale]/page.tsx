bae86cb74afe791da6b4ead3ae64c9858ad69386
wingedseraph <r89647@gmail.com>
2025-09-20 23:25:17 +0300

    chore(ui): add gap between textblocks style

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -11,8 +11,8 @@ export default function Home({ params }: PageProps<'/[locale]'>) {
   const t = useTranslations('Home');
 
   return (
-    <div className="flex flex-col gap-4 text-foreground text-lg sm:text-2xl lg:text-3xl lg:leading-12">
-      <div className="gap:8 flex flex-col items-center justify-between sm:flex-row sm:gap-4">
+    <div className="flex flex-col gap-4 text-base text-foreground sm:text-kg lg:text-xl lg:leading-8">
+      <div className="flex flex-col items-center justify-between gap-4 sm:flex-row">
         <p className="text-center sm:text-left"> {t('project')}</p>
         <Image
           src="/bread-walk.jpg"
@@ -22,7 +22,7 @@ export default function Home({ params }: PageProps<'/[locale]'>) {
           className="h-auto w-full max-w-sm sm:max-w-md lg:max-w-lg"
         />
       </div>
-      <div className="flex flex-col items-center justify-end gap-4 sm:flex-row sm:justify-center">
+      <div className="flex-end flex-col items-center justify-end gap-4 sm:flex sm:flex-row sm:justify-center">
         <Image
           src="/bread-green.jpg"
           alt="bread-hardtime image"
