049a46c9fb759d864cc5b182b99c6a6dc6d0f40c
Rob Marvin <fenalfan@gmail.com>
2025-09-21 19:04:32 +0300

    chore: rename auth service

--- src/__mocks__/firebaseAuthService.mock.ts
+++
@@ -1,8 +0,0 @@
-import { vi } from 'vitest';
-
-export const mockAuthService = {
-  logInWithEmailAndPassword: vi.fn(),
-  registerWithEmailAndPassword: vi.fn(),
-  sendPasswordReset: vi.fn(),
-  logout: vi.fn(),
-};
