a7793f95036d63747e45562670e22409f8a8dc18
wingedseraph <r89647@gmail.com>
2025-10-04 02:23:07 +0300

    fix(test): remove test from landing page, cannot test SSG properly

--- src/__mocks__/renderWithIntl.tsx
+++ src/__mocks__/renderWithIntl.tsx
@@ -1,10 +1,10 @@
-import type React from 'react';
+import type { ReactElement } from 'react';
 
 import en from '../../messages/en.json';
 import { render } from '@testing-library/react';
 import { NextIntlClientProvider } from 'next-intl';
 
-export function renderWithIntl(ui: React.ReactElement) {
+export function renderWithIntl(ui: ReactElement) {
   return render(
     <NextIntlClientProvider locale="en" messages={en}>
       {ui}
