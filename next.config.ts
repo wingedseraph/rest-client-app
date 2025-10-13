f39e7b64d3fd2b696f3d56df5c2393ae4e86bf4f
wingedseraph <r89647@gmail.com>
2025-10-13 12:17:51 +0300

    chore(next): add react-compiler

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,12 @@ import type { NextConfig } from 'next';
 
 import createNextIntlPlugin from 'next-intl/plugin';
 
-const nextConfig: NextConfig = { reactStrictMode: true };
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    reactCompiler: true,
+  },
+};
 
 const withNextIntl = createNextIntlPlugin();
 export default withNextIntl(nextConfig);
